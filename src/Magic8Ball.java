764cb477ff16c2fbdc5b58ced91b7316c2bce2d8
Dimitri King <Dimitri.king.2022@student.lhschools.org>
2018-09-13 07:36:26 -0400

    finished up intellij version of magic8ball

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -18,11 +18,32 @@ public class Magic8Ball {
         if (inputNumber <= 10) {
             System.out.println("NO!");
         }
-        else if (11<=inputNumber) && (inputNumber <= 20) {
+        else if ((11 <= inputNumber) && (inputNumber <= 20)) {
             System.out.println("Yes");
         }
-        else if (21 <=inputNumber) && (inputNumber <= 30) {
-            System.out.println("Blyat");
+        else if ((21 <= inputNumber) && (inputNumber <= 30)) {
+            System.out.println("Good idea");
+        }
+        else if ((31 <= inputNumber) && (inputNumber <= 40)) {
+            System.out.println("Bad idea");
+        }
+        else if ((41 <= inputNumber) && (inputNumber <= 50)) {
+            System.out.println("Outlook looks great.");
+        }
+        else if ((51 <= inputNumber) && (inputNumber <= 60)) {
+            System.out.println("Outlook looks bad.");
+        }
+        else if ((61 <= inputNumber) && (inputNumber <= 70)) {
+            System.out.println("Do it");
+        }
+        else if ((71 <= inputNumber) && (inputNumber <= 80)) {
+            System.out.println("Do not do it.");
+        }
+        else if ((80 <= inputNumber) && (inputNumber <= 90)) {
+            System.out.println("It will end out well.");
+        }
+        else if ((90 <= inputNumber) && (inputNumber <= 100)) {
+            System.out.println("It will end out horribly");
         }
 
 
