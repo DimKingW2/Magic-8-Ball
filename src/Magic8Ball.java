30b3c58afc207e47a3ee350913ae99e1c3785ed8
Dimitri King <Dimitri.king.2022@student.lhschools.org>
2018-10-04 07:22:04 -0400

    updated magic 8 ball

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -6,44 +6,47 @@ public class Magic8Ball {
         Scanner keyboard;
         String question;
         int inputNumber;
+        String an;
 
         //gathering information
         keyboard = new Scanner(System.in);
-        System.out.println("What is your question for this magic machine?");
-        question = keyboard.nextLine();
-        System.out.println("Now for this to work pick a number between 1-100.");
-        inputNumber = keyboard.nextInt();
 
-        //Gets your future
-        if (inputNumber <= 10) {
-            System.out.println("NO!");
-        }
-        else if ((11 <= inputNumber) && (inputNumber <= 20)) {
-            System.out.println("Yes");
-        }
-        else if ((21 <= inputNumber) && (inputNumber <= 30)) {
-            System.out.println("Good idea");
-        }
-        else if ((31 <= inputNumber) && (inputNumber <= 40)) {
-            System.out.println("Bad idea");
-        }
-        else if ((41 <= inputNumber) && (inputNumber <= 50)) {
-            System.out.println("Outlook looks great.");
-        }
-        else if ((51 <= inputNumber) && (inputNumber <= 60)) {
-            System.out.println("Outlook looks bad.");
-        }
-        else if ((61 <= inputNumber) && (inputNumber <= 70)) {
-            System.out.println("Do it");
-        }
-        else if ((71 <= inputNumber) && (inputNumber <= 80)) {
-            System.out.println("Do not do it.");
-        }
-        else if ((80 <= inputNumber) && (inputNumber <= 90)) {
-            System.out.println("It will end out well.");
-        }
-        else if ((90 <= inputNumber) && (inputNumber <= 100)) {
-            System.out.println("It will end out horribly");
+        do {
+            System.out.println("What is your question for this magic machine?");
+            question = keyboard.nextLine();
+            inputNumber = (int) (Math.random() * 100);
+
+            //Gets your future
+            if (inputNumber <= 10) {
+                   System.out.println("NO!");
+            }if ((11 <= inputNumber) && (inputNumber <= 20)) {
+                System.out.println("Yes");
+            } else if ((21 <= inputNumber) && (inputNumber <= 30)) {
+                System.out.println("Good idea");
+            } else if ((31 <= inputNumber) && (inputNumber <= 40)) {
+                System.out.println("Bad idea");
+            } else if ((41 <= inputNumber) && (inputNumber <= 50)) {
+                System.out.println("Outlook looks great.");
+            } else if ((51 <= inputNumber) && (inputNumber <= 60)) {
+                System.out.println("Outlook looks bad.");
+            } else if ((61 <= inputNumber) && (inputNumber <= 70)) {
+                System.out.println("Do it");
+            } else if ((71 <= inputNumber) && (inputNumber <= 80)) {
+                System.out.println("Do not do it.");
+            } else if ((80 <= inputNumber) && (inputNumber <= 90)) {
+                System.out.println("It will end out well.");
+            } else if ((90 <= inputNumber) && (inputNumber <= 100)) {
+                System.out.println("It will end out horribly");
+            }
+            System.out.println("Do you have another question? (type yes or no)");
+            an = keyboard.nextLine().toLowerCase();
+
+        }while(an.equals("yes"));
+
+        if (!an.equals("yes")) {
+            System.out.println("Very well.");
+            keyboard.close();
+            System.exit(0);
         }
 
 
